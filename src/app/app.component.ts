2ad2f9c6484dc17a74b6ca70ee770037f56bbad2
Kenny Nguyen <kennynguyen@codeweavers.net>
2024-01-23 20:46:02 +0000

    Property Binding Syntax
    Showcasing how the double curly braces interpolation syntax is simply sugar syntax, which is a nicer way to display values without binding to a property directly. And the way Angular does this under the hood is the same as the innerText property binding. Also binding the value of message to the property of value to show how to use one way data binding syntax correctly.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,9 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-root',
   template: ` 
     <div class="app">
-      {{ message.length ? message : 'Nothing here...' }}
+      <h1>{{ message.length ? message : 'Nothing here...' }}</h1>
+      <h1 [innerText]="message.length ? message : 'Nothing here...'"></h1>
+      <input [value]="message">
     </div> 
   `,
   styles: [
