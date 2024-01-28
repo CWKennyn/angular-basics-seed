d4fed35985518f4595e1699154bca8d5918bc53f
Kenny Nguyen <kennynguyen@codeweavers.net>
2024-01-28 18:40:16 +0000

    Use the NgOnInit Lifecycle Hook
    Started creating some template data and rendering it out in the DOM using the NgOnInit lifecycle hook. The idea of having the data within inner <div> is that this is a container component, this component is going to contain sub components (child components). The array of data will be passed down and render all of those components. For now the data will be built out, then break it down into smaller pieces. Then work out as we go how the flow of things actually works.

--- src/app/admin/containers/donut-list/donut-list.component.ts
+++ src/app/admin/containers/donut-list/donut-list.component.ts
@@ -3,15 +3,47 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'donut-list',
   template: `
-    <p>
-      donut-list works!
-    </p>
+    <div>
+      <div>
+        {{ donut.name }}
+        {{ donut.price }}
+      </div>
+    </div>
   `,
   styles: [
   ]
 })
 export class DonutListComponent implements OnInit {
+  donut!: any;
+  donuts!: any[];
+
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.donuts = [
+      {
+        id: 'abc001',
+        name: 'Just Chocolate',
+        icon: 'just-chocolate',
+        price: 119,
+        description: 'For the pure chocoholic'
+      },
+      {
+        id: 'abc002',
+        name: 'Glazed Fudge',
+        icon: 'glazed-fudge',
+        price: 129,
+        description: 'Sticky perfection'
+      },
+      {
+        id: 'abc003',
+        name: 'Caramel Swirl',
+        icon: 'caramel-swirl',
+        price: 119,
+        description: 'Chocolate drizzled with caramel'
+      }
+    ];
+
+    this.donut = this.donuts[0];
+  }
 }
