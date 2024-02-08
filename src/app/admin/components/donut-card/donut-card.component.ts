38ae7f4a273bf04b05b6808cb7323f2b97ce723c
Kenny Nguyen <kennynguyen@codeweavers.net>
2024-02-08 21:04:31 +0000

    Complex Inline Styles with NgStyle
    Using first directive which is a way we can bind specific behaviour to an element or a component. In this case we are binding to an element via ngStyle directive. Which is a more super-powered style binding which allows us to specify an object, and get a bit more complex when we need to set multiple inline styles. Another style that can be demostrated would be adding:
    'font-size.px' : 20
    Which assumes that we get data back from a server of the number 20. This method allows for specifying the unit as an extension of the property we want to use which in this case is pixels.

--- src/app/admin/components/donut-card/donut-card.component.ts
+++ src/app/admin/components/donut-card/donut-card.component.ts
@@ -6,7 +6,10 @@ import { Donut } from '../../models/donut.model';
   template: `
     <div 
       class="donut-card"
-      [style.border]="donut.promo ? '2px solid #eee' : 'none'">
+      [ngStyle]="{
+        border: donut.promo ? '2px solid #eee' : 'none'
+      }"
+    >
       <img 
         src="/assets/img/{{ donut.icon }}.svg"
         [alt]="donut.name"
