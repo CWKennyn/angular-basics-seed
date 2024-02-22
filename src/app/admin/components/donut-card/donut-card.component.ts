f32d5cd9b110856698bf04536fca6b3bb53516ea
Kenny Nguyen <kennynguyen@codeweavers.net>
2024-02-22 20:43:57 +0000

    Advanced NgSwitch Syntax with <ng-template>
    Implemented a slightly more advanced use case by using the ng-template syntax. From using ng-container and ng-template it means that it allows us to specify the word "NEW" without a containing element (basically they do not get rendered). Reason why we might want to use this method because the code is presented in a simpler fashion. Also if you have a piece of DOM (<span class="donut-card-label">) that is used in every single scenario, this might be a great way to handle things.

--- src/app/admin/components/donut-card/donut-card.component.ts
+++ src/app/admin/components/donut-card/donut-card.component.ts
@@ -18,13 +18,11 @@ import { Donut } from '../../models/donut.model';
       <div>
         <p class="donut-card-name">
           {{ donut.name }}
-          <ng-container [ngSwitch]="donut.promo"]>
-            <span *ngSwitchCase="'new'" class="donut-card-label">NEW</span>
-            <span *ngSwitchCase="'limited'" class="donut-card-label"
-              >LIMITED</span
-            >
-            <span *ngSwitchDefault class="donut-card-label">
-              Nothing special...
+          <ng-container [ngSwitch]="donut.promo">
+            <span class="donut-card-label">
+              <ng-template [ngSwitchCase]="'new'"> NEW </ng-template>
+              <ng-template [ngSwitchCase]="'limited'"> LIMITED </ng-template>
+              <ng-template ngSwitchDefault> Nothing special... </ng-template>
             </span>
           </ng-container>
         </p>
