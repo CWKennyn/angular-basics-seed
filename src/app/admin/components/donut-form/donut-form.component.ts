a01cbe4708e680d0d4fb0ce6fdc84304405a5984
Kenny Nguyen <kennynguyen@codeweavers.net>
2024-03-06 16:55:13 +0000

    <select> and <option> Rendering and Binding
    Creating a select icon menu and binding it to Angular. Within the component definition, we are adding the icons property which contains a string array of the names of the donut icons. Using [ngValue] because we can set values that are not strings:
    [ngValue]="2" the value will be bound as a number. [value]="2" the value will be bound as a string.

--- src/app/admin/components/donut-form/donut-form.component.ts
+++ src/app/admin/components/donut-form/donut-form.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'donut-form',
@@ -7,6 +7,15 @@ import { Component } from '@angular/core';
       <label>
         <span>Name</span>
         <input type="text" name="name" class="input" ngModel />
+      </label>
+
+      <label>
+        <span>Icon</span>
+        <select name="icon" class="input input--select" ngModel>
+          <option *ngFor="let icon of icons" [ngValue]="icon">
+            {{ icon }}
+          </option>
+        </select>
       </label>
 
       <label>
@@ -55,6 +64,20 @@ import { Component } from '@angular/core';
     `
   ]
 })
-export class DonutFormComponent {
+export class DonutFormComponent implements OnInit {
 
+  icons: string[] = [
+    'caramel-swirl',
+    'glazed-fudge',
+    'just-chocolate',
+    'sour-supreme',
+    'strawberry-glaze',
+    'vanilla-sundae',
+    'zesty-lemon',
+  ];
+  constructor() {}
+
+  ngOnInit(): void {
+    
+  }
 }
