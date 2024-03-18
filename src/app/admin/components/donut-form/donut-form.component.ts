9820d2e667275f7275f8f9b1dfe15b982c18f571
Kenny Nguyen <kennynguyen@codeweavers.net>
2024-03-18 17:09:31 +0000

    Trigger Validation Messages On Submit
    Enhancing the user experience by making it so that if any of the input fields of the form are invalid, when clicking the "Create" button, will mark all as touched and display the relevant error messages

--- src/app/admin/components/donut-form/donut-form.component.ts
+++ src/app/admin/components/donut-form/donut-form.component.ts
@@ -28,40 +28,76 @@ import { NgForm } from '@angular/forms';
 
       <label>
         <span>Icon</span>
-        <select name="icon" class="input input--select" required ngModel>
+        <select 
+          name="icon" 
+          class="input input--select" 
+          required ngModel 
+          #icon="ngModel" 
+        >
           <option *ngFor="let icon of icons" [ngValue]="icon">
             {{ icon }}
           </option>
         </select>
+        <ng-container *ngIf="icon.invalid && icon.touched">
+          <div class="donut-form-error" *ngIf="icon.errors?.required">
+            Icon is required.
+          </div>
+        </ng-container>
       </label>
 
       <label>
         <span>Price</span>
-        <input type="number" name="price" class="input" required ngModel />
+        <input 
+          type="number" 
+          name="price" 
+          class="input" 
+          required 
+          ngModel 
+          #price="ngModel" 
+        />
+        <ng-container *ngIf="price.invalid && price.touched">
+          <div class="donut-form-error" *ngIf="price.errors?.required">
+            Price is required.
+          </div>
+        </ng-container>
       </label>
 
       <div class="donut-form-radios">
         <p class="donut-form-radios-label">Promo:</p>
         <label>
-          <input type="radio" name="promo" required [value]="undefined" ngModel />
+          <input 
+            type="radio" 
+            name="promo"  
+            [value]="undefined" 
+            ngModel />
           <span>None</span>
         </label>
         <label>
-          <input type="radio" name="promo" required value="new" ngModel />
+          <input type="radio" name="promo" value="new" ngModel />
           <span>New</span>
         </label>
         <label>
-          <input type="radio" name="promo" required value="limited" ngModel />
+          <input type="radio" name="promo" value="limited" ngModel />
           <span>Limited</span>
         </label>
       </div>
 
       <label>
         <span>Description</span>
-        <textarea name="description" class="input input--textarea" required ngModel></textarea>
+        <textarea 
+          name="description" 
+          class="input input--textarea" 
+          required 
+          ngModel
+          #description="ngModel"></textarea>
+        <ng-container *ngIf="description.invalid && description.touched">
+          <div class="donut-form-error" *ngIf="description.errors?.required">
+            Description is required.
+          </div>
+        </ng-container>
       </label>
 
-      <button type="submit" class="btn btn--green" [disabled]="form.invalid">
+      <button type="submit" class="btn btn--green">
         Create
       </button>
 
@@ -110,6 +146,8 @@ export class DonutFormComponent {
   handleSubmit(form: NgForm) {
     if(form.valid) {
       console.log(form.value);
+    } else {
+      form.form.markAllAsTouched();
     }
   }
 }
