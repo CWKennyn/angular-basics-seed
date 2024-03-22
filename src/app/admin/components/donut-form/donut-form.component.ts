61a08cae6a4c9f8dd21e6e701235c8086a3f36f6
Kenny Nguyen <kennynguyen@codeweavers.net>
2024-03-22 17:04:01 +0000

    Show Visual Feedback on Form Submission
    Adding a little enhancement to the form which allows us to show to the user that something is happening in the background when they submit the form until something like a API request comes back.

--- src/app/admin/components/donut-form/donut-form.component.ts
+++ src/app/admin/components/donut-form/donut-form.component.ts
@@ -100,7 +100,11 @@ import { NgForm } from '@angular/forms';
 
       <button type="submit" class="btn btn--green">Create</button>
       <button type="button" class="btn btn--grey" (click)="form.resetForm()">Reset Form</button>
-      {{ form.submitted }}
+      
+      <div class="donut-form-working" *ngIf="form.valid && form.submitted">
+        Working...
+      </div>
+
       <pre>{{ form.value | json }}</pre>
     </form>
   `,
@@ -121,6 +125,11 @@ import { NgForm } from '@angular/forms';
               margin-bottom: 0;
             }
           }
+        }
+        &-working {
+          font-size: 12px;
+          font-style: italic;
+          margin: 10px 0;
         }
         &-error {
           font-size: 12px;
