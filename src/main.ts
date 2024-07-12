d7876462ed9b39d6e44a497dadc799f30f34ea48
Kenny Nguyen <Kenny.Nguyen@coxautoinc.com>
2024-07-12 15:10:49 +0100

    Providing a Route Config via importProvidersFro
    Covered using "importProvidersFrom" the RouterModule to allow us then inject some of the built in Angular modules because the RouterModule is not a component. This will also apply to some other built in Angular modules. This is a standalone approach for the time being and how to achieve a fully standalone application. Refactored "app.module" into a "app.routes". Have a very nice and clean "app.routes" file with just a routing definition and a nice const that is exported, and nothing else.

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,16 @@
-import { enableProdMode } from '@angular/core';
+import { enableProdMode, importProvidersFrom } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app/app.component';
+import { AppRoutes } from './app/app.routes';
+
 import { environment } from './environments/environment';
 
 if (environment.production) {
   enableProdMode();
 }
 
-bootstrapApplication(AppComponent)
-  .catch((err) => console.error(err));
+bootstrapApplication(AppComponent, {
+  providers: [importProvidersFrom(RouterModule.forRoot(AppRoutes))],
+}).catch((err) => console.error(err));
